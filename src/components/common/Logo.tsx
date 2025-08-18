22dd2b95ce0f2c14a384575c692fe793c0ffeafa
Ola <aaa-dev@A.A.A>
2025-08-18 13:58:06 +0100

    Chore: Created a file to hold the logo icon since i am going to use it in two major components

---
+++ src/components/common/Logo.tsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+  return (
+    <div className="flex justify-center">
+      <img 
+        src="/assets/images/logo-full.svg" 
+        alt="Config Logo" 
+        className="w-full max-w-md"
+      />
+    </div>
+  )
+}
+
+export default Logo
