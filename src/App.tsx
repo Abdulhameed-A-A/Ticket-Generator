1c8290d22d06a0690aad4ee7a74333c2a7507e73
Ola <aaa-dev@A.A.A>
2025-08-18 13:38:39 +0100

    Chore: Created the formvalidation file in the utils folder and game it the authentication code for email in a function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 
 
 function App() {
+  // const blob = new Blob(["Hello world"], { type: "text/plain" });
+  // const fakeFile = new File([blob], "hello.txt", { type: "text/plain" });
 
+  // validateForm(fakeFile)
 
   return (
     <div></div>
